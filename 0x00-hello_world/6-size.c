c2f16ee12740d82565413aee98c3920f0c185f37
saraa93-bit <saraa93177228@gmail.com>
2023-09-07 17:00:09 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
   */
 int main(void)
 {
-printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
