a1dbca28dcf80a41ec0ac6ab4e42f10ec72e731d
saraa93-bit <saraa93177228@gmail.com>
2023-09-08 14:42:21 -0700

    4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Description: print numbers
+*
+* return: 0 is success
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
