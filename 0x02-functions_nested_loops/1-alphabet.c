d3286ffc94331b11ebcee5bfa740a08158fe4009
saraa93-bit <saraa93177228@gmail.com>
2023-09-12 04:00:03 -0700

    o

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Description: print_alphabet in lower case
+*
+* Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int -putchar(char c);
+void print_alphabet(void);
+#endif
