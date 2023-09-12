752673e48454cd8f9191899002cf1e6a1ff9403d
saraa93-bit <saraa93177228@gmail.com>
2023-09-12 12:05:15 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* print_alphabet_x10
+* Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+putchar(ch);
+putchar('\n');
+
+	}
+	}
