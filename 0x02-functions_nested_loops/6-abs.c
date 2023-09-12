08b37c9f10f9fe146a61ca773631b096b90fd277
saraa93-bit <saraa93177228@gmail.com>
2023-09-12 15:35:06 -0700

    5

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,9 +6,17 @@
 *
 *Return: Always 0 (success)
 */
-int _abs(int)
+
+int _abs(int c)
 {
-if (n < 0)
-	n = (-1) * n;
-return (n);
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+
+	return (c);
 }
