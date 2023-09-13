599255431ee8bcc88da3fa1b7908edfd5c4cdeae
saraa93-bit <saraa93177228@gmail.com>
2023-09-13 12:58:52 -0700

    11

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * add - Adds two values
-  * @n1: The first number to be computed
-  * @n2: The second number to be computed
-  *
-  * Return: Integer value
-  */
-int add(int n1, int n2)
+* add - Adds two values
+*
+* @a: The first number to be computed
+* @b: The second number to be computed
+*
+* Return: some of a & b
+*/
+int add(int a, int b)
 {
-	return (n1 + n2);
+int sum = a + b;
+	return (sum);
 }
