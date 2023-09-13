3b75d7ff1840abc3e1e23c84c0ee9ac9091f91bb
saraa93-bit <saraa93177228@gmail.com>
2023-09-13 11:01:53 -0700

    3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,24 +2,24 @@
 #include "main.h"
 
 /**
-  * main - Prints the sum of all multiples of 3 or 5 up to 1024
-  *
-  * Return: Always (Success)
-  */
+* main - Prints the sum of all multiples of 3 or 5 up to 1024
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
-	int i, z = 0;
+int i, z = 0;
 
-	while (i < 1024)
-	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			z += i;
-		}
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
 
-		i++;
-	}
+i++;
+}
 
-	printf("%d\n", z);
-	return (0);
+printf("%d\n", z);
+return (0);
 }
