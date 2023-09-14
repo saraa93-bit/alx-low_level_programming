1c05aecbd7a74b86f0c0c0efa7dc508193edfd8f
saraa93-bit <saraa93177228@gmail.com>
2023-09-14 08:26:25 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,16 +4,15 @@
 */
 void more_numbers(void)
 {
-	int i;
-	char c;
+	int i, c;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 	for (c = 0; c <= 14; c++)
 	{
 	if (c > 9)
-		putchar((c / 10) + '0');
-	putchar((c % 10) + '0');
+		putchar(1 + 48);
+	putchar((c % 10) + 48);
 	}
 	putchar('\n');
 	}
