40e44fd7e350505e5a27e3dfcb6ecf9e419bb6e5
saraa93-bit <saraa93177228@gmail.com>
2023-09-14 11:21:08 -0700

    6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,27 +3,26 @@
 * print_diagonal - draws a diagonal line
 * @n: takes in an integer
 */
-
 void print_diagonal(int n)
 {
-int prior, i;
+	int i, j;
 
 	if (n <= 0)
-	{
-		putchar('\n');
-		return;
-	}
-
-	for (i = 0 ; i < n ; i++)
-	{
-		prior = i;
-		while (prior > 0)
-		{
-			_putchar(32);
-			prior--;
-		}
-		_putchar('\\');
 		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
 	}
 }
 
