7c1339e367a731b7370f8222e11342a3341b16e4
saraa93-bit <saraa93177228@gmail.com>
2023-09-14 12:36:02 -0700

    4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 
 int _isupper(int c);
+int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -11,5 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+void print_number(int n);
 #endif
