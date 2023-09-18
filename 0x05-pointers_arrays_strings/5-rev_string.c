be7f45e8f3a97de7ccdcfa286e725a3ae237d6ae
saraa93-bit <saraa93177228@gmail.com>
2023-09-18 14:06:19 -0700

    3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,8 @@ void rev_string(char *s)
 int i, l;
 char x;
 for (l = 0; s[l] != '\0'; l++)
-for (i = 0; i < l / 2; i++)
+
+	for (i = 0; i < l / 2; i++)
 	x = s[i];
 	s[i] = s[l - 1 - i];
 	s[l - 1 - i] = x;
