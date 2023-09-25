38f97a7a2ca660b205c8b16e47f8d29ed1a54ead
saraa93-bit <saraa93177228@gmail.com>
2023-09-25 06:54:31 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+
+/**
+ * main - Print the word "main"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
