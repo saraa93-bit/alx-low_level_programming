af6f303e0ebeadb8690b5c7d4c50d234a116fc22
saraa93-bit <saraa93177228@gmail.com>
2023-10-10 03:22:29 -0700

    0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+
+/**
+* Create a header file that defines a macro named SIZE.
+*
+* return: always 0 (success)
+*/
+
+#define size 1024
