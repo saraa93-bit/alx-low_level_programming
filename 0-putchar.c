9c0e562b18b811d857a40b52010bbcce82e50e66
saraa93-bit <saraa93177228@gmail.com>
2023-10-11 13:47:56 -0700

    0

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+char p[] = "_putchar";
+for (i = 0;i < 8; i++)
+{
+	_putchar(p[i]);
+}
+_putchar('\n');
+return (0);
+}
+
