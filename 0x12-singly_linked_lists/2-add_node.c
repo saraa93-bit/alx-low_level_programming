59be017a719bec17ef9d34c274aad1916861dec8
saraa93-bit <saraa93177228@gmail.com>
2023-10-23 11:56:12 -0700

    kj

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,49 +1,48 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
-* _strlen - gets length of the string
-* @s: string
-* Return: the string length.
-*/
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
+ */
 int _strlen(const char *s)
 {
 	int i;
 
 	for (i = 0; s[i]; i++)
-
+		;
 	return (i);
 }
 /**
-* add_node - add new nodes to the list
-* @head: current place in the list
-* @str: a string to add to the head
-* Return: pointer to the current position in list
-*/
+ * add_node - add new nodes to the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: pointer to current position in list
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	int i, len;
-	char *cont;
-	list_t *list;
+	char *content;
+	list_t *new;
 
 	if (str == NULL || head == NULL)
 		return (NULL);
 	len = _strlen(str);
-	list = *head;
-	cont = malloc((len + 1) * sizeof(char));
-	if (cont == NULL)
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-		cont[i] = str[i];
-	list = malloc(sizeof(list_t));
-	if (list == NULL)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(cont);
+		free(content);
 		return (NULL);
 	}
-	list->str = cont;
-	list->len = len;
-	list->next = *head;
-	*head = list;
-	return (list);
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
