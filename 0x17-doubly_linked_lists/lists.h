09883c7eee8a4c8dbd7f0c6fe483c84d70f92f3c
saraa93-bit <saraa93177228@gmail.com>
2023-12-09 08:36:43 -0800

    k

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+size_t print_dlistint(const dlistint_t *h);
+
