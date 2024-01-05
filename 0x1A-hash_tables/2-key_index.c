b85de1c5c99432bab4ba7e570a8cc82f0e11f2bd
saraa93-bit <saraa93177228@gmail.com>
2024-01-05 12:15:28 +0200

    classes

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,12 +12,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-    
-    if (key == NULL || size == 0)
-    return (0);
-    
-    hash_value = hash_djb2(key);
-    
-    return (hash_value % size);
+	return (hash_djb2(key) % size);
 }
